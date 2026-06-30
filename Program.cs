37ca62c3593fce6504e996129a87c39035b7a4d0
wesleysotnas64 <wesleysotnas64@email.com>
2026-06-29 22:18:50 -0300

    fix: CORS error;

--- Program.cs
+++ Program.cs
@@ -13,28 +13,42 @@ if (string.IsNullOrEmpty(connectionString))
 {
     throw new InvalidOperationException("A variável de ambiente 'DB_CONNECTION_STRING' não está definida.");
 }
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
-// Add services to the container.
+// 1. ADICIONADO: Configuração da política de CORS para liberar qualquer origem
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapibuilder
 builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<TournamentService>();
+builder.Services.AddScoped<UserTournamentService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-/*if (app.Environment.IsDevelopment())
-{*/
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-//}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
+
+// 2. ADICIONADO: Ativa o middleware do CORS usando a política que criamos acima
+// IMPORTANTE: Deve ficar antes do UseAuthorization e MapControllers
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
